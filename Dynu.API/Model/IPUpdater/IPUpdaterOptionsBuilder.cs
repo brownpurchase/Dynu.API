c54ea3f7ef38ab3726ee6cfa063392e79cd6f381
Telly Brown <2632056+tellybrown@users.noreply.github.com>
2018-06-03 01:40:23 -0500

    Add files via upload

---
+++ Dynu.API/Model/IPUpdater/IPUpdaterOptionsBuilder.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Dynu.API.Model.IPUpdater
+{
+    public class IPUpdaterOptionsBuilder
+    {
+        public PortOption Port { get; private set; }
+        public IAuth Authentication { get; private set; }
+        public string Protocol { get; private set; }
+        public string UserAgent { get; private set; }
+
+        public IPUpdaterOptionsBuilder()
+        {
+            WithAuthentication(new BasicHeaderAuth("", ""));
+            WithPort(PortOption.Port443);
+            WithUserAgent(".NET Dynu.API");
+        }
+
+        public IPUpdaterOptionsBuilder WithPort(PortOption port)
+        {
+            Port = port;
+            switch (Port)
+            {
+                case PortOption.Port443:
+                    Protocol = "https";
+                    break;
+                case PortOption.Port80:
+                case PortOption.Port8245:
+                    Protocol = "http";
+                    break;
+            }
+            return this;
+        }
+        public IPUpdaterOptionsBuilder WithAuthentication(IAuth authentication)
+        {
+            Authentication = authentication;
+            return this;
+        }
+        public IPUpdaterOptionsBuilder WithUserAgent(string userAgent)
+        {
+            UserAgent = userAgent;
+            return this;
+        }
+    }
+}
---
+++ Dynu.API/Model/IPUpdater/PortOption.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dynu.API.Model.IPUpdater
+{
+    public enum PortOption
+    {
+        Port443,
+        Port80,
+        Port8245
+    }
+}
