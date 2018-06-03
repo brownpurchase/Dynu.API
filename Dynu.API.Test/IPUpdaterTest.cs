8dfe9962e1dac8ed9bb0e428d4ea30e49c46a6b6
Telly Brown <2632056+tellybrown@users.noreply.github.com>
2018-06-03 01:44:43 -0500

    Update IPUpdaterTest.cs

--- Dynu.API.Test/IPUpdaterTest.cs
+++ Dynu.API.Test/IPUpdaterTest.cs
@@ -10,7 +10,9 @@ namespace Dynu.API.Test
         [InlineData("username", "password", "test.com", "subdomain")]
         public async Task UpdateIP(string username, string password, string domain, string subdomain)
         {
-            IPUpdater updater = new IPUpdater(new BasicHeaderAuth(username,  password));
+            var options = new IPUpdaterOptionsBuilder()
+                .WithAuthentication(new BasicHeaderAuth(username, password));
+            IPUpdater updater = new IPUpdater(options);
             await updater.UpdateSubDomain(domain, subdomain, null, null);
         }
     }
